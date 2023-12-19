fa8bae152532cb466ceacea53ab35fd9b940534f
Susko3 <Susko3@protonmail.com>
2023-12-19 02:46:27 +0100

    Handle IME composition on Android
    After leading lots of AOSP code of `BaseInputConnection`, `EditableInputConnection`, etc.
    I've arrived at this solution.

--- osu.Framework.Android/Input/AndroidInputConnection.cs
+++ osu.Framework.Android/Input/AndroidInputConnection.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Diagnostics;
+using System.Threading;
 using Android.Views;
 using Android.Views.InputMethods;
 using Java.Lang;
@@ -11,10 +13,55 @@ namespace osu.Framework.Android.Input
     {
         private readonly AndroidGameView targetView;
 
+        private volatile int batchNestingLevel;
+
         public AndroidInputConnection(AndroidGameView targetView, bool fullEditor)
             : base(targetView, fullEditor)
         {
             this.targetView = targetView;
+        }
+
+        public override bool BeginBatchEdit()
+        {
+            Interlocked.Increment(ref batchNestingLevel);
+            return true;
+        }
+
+        public override bool EndBatchEdit()
+        {
+            Debug.Assert(batchNestingLevel > 0, $"{nameof(EndBatchEdit)} called without preceding {nameof(BeginBatchEdit)} call");
+
+            int newValue = Interlocked.Decrement(ref batchNestingLevel);
+
+            // only check for composition after all nested editing batches have ended
+            if (newValue == 0)
+                invokeComposingEventIfComposingActive();
+
+            return newValue > 0;
+        }
+
+        /// <summary>
+        /// Invokes <see cref="AndroidGameView.ComposingText"/> if there is an active composition.
+        /// </summary>
+        private void invokeComposingEventIfComposingActive()
+        {
+            var editable = Editable;
+            if (editable == null)
+                return;
+
+            int start = GetComposingSpanStart(editable);
+            int end = GetComposingSpanEnd(editable);
+
+            if (start == -1 || end == -1)
+                return;
+
+            int left = Math.Min(start, end);
+            int right = Math.Max(start, end);
+            string composition = editable.SubSequence(left, right);
+
+            // TODO: get selection within the composition
+            // for now, let's just have the selection cursor at the end of the composition string
+            targetView.OnComposingText(composition, composition.Length, 0);
         }
 
         public override bool CommitText(ICharSequence? text, int newCursorPosition)
